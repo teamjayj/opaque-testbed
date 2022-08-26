b8c3aab5e504c9052918e1a6b9bb19db09f2755a
JG Rivera <rivera.jga10@gmail.com>
2022-08-26 18:23:31 +0800

    feat: add potls test scripts and improve structure

---
+++ apps/load-generator/src/potls/potls-client-login.test.ts
@@ -0,0 +1,23 @@
+import { loadCSV, getUrlWithHost, requestParams } from "../common";
+import { PotlsClientLoginRegister } from "./types";
+import { sleep, check } from "k6";
+import execution from "k6/execution";
+import http from "k6/http";
+
+const csvData = loadCSV("potls-client-login-register.csv");
+
+export default function () {
+    const {
+        user_id: username,
+        plaintext_password: password,
+    }: PotlsClientLoginRegister = csvData[execution.vu.idInTest - 1];
+
+    const user = JSON.stringify({ username, password });
+    const response = http.post(getUrlWithHost("login"), user, requestParams);
+
+    check(response, {
+        "status is 200": () => response.status === 200,
+    });
+
+    sleep(1);
+}
