961a03b310f9a969cc7dc41d97f8caaf230ffcac
JG Rivera <rivera.jga10@gmail.com>
2022-08-26 17:36:29 +0800

    feat: decouple database from app

--- apps/potls-express-server/src/app.ts
+++ apps/potls-express-server/src/app.ts
@@ -1,14 +1,7 @@
 import "dotenv/config";
-import express, { NextFunction, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt";
 import cors from "cors";
-
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-
-const userDatabase: Map<string, string> = new Map();
 
 function throwErrorIfEmptyUsernameOrPassword(
     username: string,
@@ -27,61 +20,78 @@ function isLoadTestingEnvironment(): boolean {
     return process.env.NODE_ENV === "load-testing";
 }
 
-app.post(
-    "/register",
-    async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const { username, password: plaintextPassword } = req.body;
+export function createApp(userDatabase: Map<string, string>): Application {
+    const app = express();
 
-            throwErrorIfEmptyUsernameOrPassword(username, plaintextPassword);
+    app.use(express.json());
+    app.use(cors());
 
-            if (userDatabase.has(username)) {
-                throw new Error(`User '${username}' is already registered`);
+    app.post(
+        "/register",
+        async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const { username, password: plaintextPassword } = req.body;
+
+                throwErrorIfEmptyUsernameOrPassword(
+                    username,
+                    plaintextPassword
+                );
+
+                if (userDatabase.has(username)) {
+                    throw new Error(`User '${username}' is already registered`);
+                }
+
+                const hashedPassword = await bcrypt.hash(plaintextPassword, 10);
+
+                if (!isLoadTestingEnvironment()) {
+                    userDatabase.set(username, hashedPassword);
+                }
+
+                return res.json({ message: "Successfully registered" });
+            } catch (error) {
+                next(error);
             }
+        }
+    );
 
-            const hashedPassword = await bcrypt.hash(plaintextPassword, 10);
+    app.post(
+        "/login",
+        async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const { username, password: plaintextPasswordAttempt } =
+                    req.body;
 
-            if (!isLoadTestingEnvironment()) {
-                userDatabase.set(username, hashedPassword);
+                throwErrorIfEmptyUsernameOrPassword(
+                    username,
+                    plaintextPasswordAttempt
+                );
+
+                const hashedPassword = userDatabase.get(username);
+
+                if (hashedPassword == null) {
+                    throw new Error(`Cannot find user: ${username}`);
+                }
+
+                const isValidLogin = await bcrypt.compare(
+                    plaintextPasswordAttempt,
+                    hashedPassword
+                );
+
+                if (isValidLogin) {
+                    return res.json({ message: "Successfully logged in" });
+                } else {
+                    return res.status(401).json({ message: "Invalid login" });
+                }
+            } catch (error) {
+                next(error);
             }
-
-            return res.json({ message: "Successfully registered" });
-        } catch (error) {
-            next(error);
         }
-    }
-);
+    );
 
-app.post("/login", async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { username, password: plaintextPasswordAttempt } = req.body;
+    app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+        console.error(err);
+        return res.status(500).json({ message: err.message });
+    });
 
-        throwErrorIfEmptyUsernameOrPassword(username, plaintextPasswordAttempt);
-
-        const hashedPassword = userDatabase.get(username);
-
-        if (hashedPassword == null) {
-            throw new Error(`Cannot find user: ${username}`);
-        }
-
-        const isValidLogin = await bcrypt.compare(
-            plaintextPasswordAttempt,
-            hashedPassword
-        );
-
-        if (isValidLogin) {
-            return res.json({ message: "Successfully logged in" });
-        } else {
-            return res.status(401).json({ message: "Invalid login" });
-        }
-    } catch (error) {
-        next(error);
-    }
-});
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err);
-    return res.status(500).json({ message: err.message });
-});
-
-export default app;
+    return app;
+}
--- apps/potls-express-server/src/server.ts
+++ apps/potls-express-server/src/server.ts
@@ -1,8 +1,10 @@
-import app from "./app";
+import { createApp } from "./app";
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
+const userDatabase: Map<string, string> = new Map();
+
+createApp(userDatabase).listen(port, () => {
     console.log(
         `PoTLS express server is listening to port ${port} with env: ${process.env.NODE_ENV}`
     );
