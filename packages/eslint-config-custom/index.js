7d18a7d068c1191fb3f1d806428a79f86da0ab66
JG Rivera <rivera.jga10@gmail.com>
2022-08-26 16:50:06 +0800

    chore: allow anonymous function lint rule

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,13 +1,13 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-    "import/no-anonymous-default-export": [
-      "error",
-      {
-        allowArrowFunction: true,
-      },
-    ],
-  },
+    extends: ["next", "turbo", "prettier"],
+    rules: {
+        "@next/next/no-html-link-for-pages": "off",
+        "react/jsx-key": "off",
+        "import/no-anonymous-default-export": [
+            "error",
+            {
+                allowAnonymousFunction: true,
+            },
+        ],
+    },
 };
